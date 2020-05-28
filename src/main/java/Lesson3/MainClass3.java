28d306209a4c40b0ca3d437ebf3161063abc230d
andreiPavlishin <andrei_pavlishin@bk.ru>
2020-05-29 00:43:56 +0300

    Lesson2

--- src/main/java/Lesson3/MainClass3.java
+++ src/main/java/Lesson3/MainClass3.java
@@ -5,22 +5,30 @@ public class MainClass3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
-        int result = (int) (Math.random() * 10 - 1);
-        System.out.println("Добро пожаловать в игру угадай число.");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Введите число от 0 до 9");
-            a = sc.nextInt();
-            if (a == result) {
-                System.out.println("Ответ верный, поздравляем");
-                break;
-            } else {
-                if (result < a) {
-                    System.out.println("Неправильно, загаданное число меньше ");
+        int userAnsver;
+        do{
+            int result = (int) (Math.random() * 10 - 1);
+            System.out.println("Добро пожаловать в игру угадай число.");
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Введите число от 0 до 9");
+                a = sc.nextInt();
+                if (a == result) {
+                    System.out.println("Ответ верный, поздравляем");
+                    break;
                 } else {
-                    System.out.println("Неправильно, загаданное число больше ");
+                    if (result < a) {
+                        System.out.println("Неправильно, загаданное число меньше ");
+                    } else {
+                        System.out.println("Неправильно, загаданное число больше ");
+                    }
                 }
             }
-        }
-        System.out.println("Ответ:" + result);
+            System.out.println("Ответ:" + result);
+            System.out.println();
+            System.out.println("Хотите повторить, нажмите 1");
+            userAnsver = sc.nextInt();
+        } while (userAnsver == 1);
+            System.out.println("До скорых встреч");
     }
 }
+
